afa7150b104372096f74d2d8a0ca9c54a0eea9c5
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-08-28 23:26:44 +0200

    Instagram, woocommerce stock count on HP, media queries

--- web/app/themes/shopkeeper-child/functions.php
+++ web/app/themes/shopkeeper-child/functions.php
@@ -2,6 +2,27 @@
 
 add_action('wp_enqueue_scripts', 'chocoThemeEnqueueStyles');
 add_action('nav_menu_css_class', 'chocAddIconToMenuItem', 10, 2);
+add_shortcode('choc_stock_amount', 'displayProductsLeftInStock');
+
+function displayProductsLeftInStock($atts)
+{
+    $a = shortcode_atts([
+        'product_id' => 1,
+    ], $atts);
+
+    $stock = get_post_meta( $a['product_id'], '_stock', true );
+
+    echo '<p class="left-in-stock"><span class="stock-amount">' . (int) $stock . ' left in stock' . '</span></p>';
+}
+
+function bartag_func( $atts ) {
+    $a = shortcode_atts( array(
+        'foo' => 'something',
+        'bar' => 'something else',
+    ), $atts );
+
+    return "foo = {$a['foo']}";
+}
 
 function chocAddIconToMenuItem($classes, $item)
 {
@@ -22,10 +43,10 @@ function chocoThemeEnqueueStyles()
         array($parent_style)
     );
 
-//    wp_enqueue_style('font-awesome',
-//        get_stylesheet_directory_uri() . '/bower_components/fontawesome/css/font-awesome.min.css',
-//        array('child-style')
-//    );
+    wp_enqueue_style('media-queries',
+        get_stylesheet_directory_uri() . '/media-queries.css',
+        array('child-style')
+    );
 
     if (is_front_page()) {
         wp_enqueue_style('hp-style',
